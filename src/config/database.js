31ae12724c8e29314d3c68eff39453da67716ce2
Fernando Henriques <fernando.trabalhos@gmail.com>
2018-09-01 19:21:11 -0300

    Fix Mongoose connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/chat-api');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/chat', { useNewUrlParser: true });
 
 module.exports = mongoose.connection;
